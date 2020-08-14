c38adbf706cd764c65eed48e22f24eae569808c1
RaHoRaHo <ralf.hoyer89@gmx.de>
2020-08-14 12:58:59 +0200

    Kleine Änderungen. CSV-Datei mit dabei. Ein Monster vollständig gescraped und in csv gespeicher

--- 5eTools.py
+++ 5eTools.py
@@ -5,8 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import os
 from time import sleep
+import csv
 
-#Bei jedem neuen Durchlauf muss das geöffnete Browser Fenster wieder geschlossen werden
+
 
 start_url = "https://5e.tools/bestiary.html#aarakocra_mm"
 gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))  #legt den driver in PATH ab
@@ -17,11 +18,27 @@ wait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='onet
 wait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='stat-tab  btn btn-default stat-tab-gen']"))).click()   #wartet bis die seite geladen ist und klickt auf den Info-Block
 sleep(10)   #manchmal bekomme ich die Fehlermeldung, dass etwas noch nicht gefunden werden kann, hier pausiert das programm um sicher zu gehen, dass die seite geladen ist
 
-p_element = driver.find_element_by_xpath("//div[@class='rd__b  rd__b--2']//descendant-or-selfc::*")   #xpath holt das gesuchte element
+description = driver.find_element_by_xpath("//div[@class='rd__b  rd__b--2']//descendant-or-self::*")   #xpath holt das gesuchte element
+name = driver.find_element_by_xpath("//span[@class='ecgen__name bold col-4-2 pl-0']/self::*")
+cr = driver.find_element_by_xpath("//span[@class='col-1-7 text-center']/self::*")
+source_element = driver.find_element_by_xpath("//span[@class='col-2 text-center sourceMM pr-0']")
+source = source_element.get_attribute("title")
+print(description.text)
+print(name.text)
+print(cr.text)
+print(source)
 
 
+with open(r'C:\Users\ralfh\Documents\Python Scripts\DH1_Project\DH1_Project\Monsterliste.csv', mode='w') as monsterlist:
+    writer = csv.DictWriter(monsterlist, fieldnames = ["Name", "CR", "Source", "Beschreibung"])
+    writer.writeheader()
+    monsterlist = csv.writer(monsterlist, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+    monsterlist.writerow([name.text, cr.text, source, description.text])
+    
 
-print(p_element.text)
+
+driver.close()
 #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 #"//div[@class='rd__b  rd__b--2']/p[1]"
 ####
