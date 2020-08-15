c45ce22c18ad50fa8f3f3dfd060c01503c7f955f
RaHoRaHo <ralf.hoyer89@gmx.de>
2020-08-15 07:46:27 +0200

    CSV Update: Source implementiert, scheint zu funktionieren! Sonderzeichen werden komisch angezeigt, evtl gibts da ne Encoding Lösung. Bitte mit auf die ToDo setzen

--- 5eTools.py
+++ 5eTools.py
@@ -6,9 +6,6 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import os
 from time import sleep
 import csv
-import re
-
-
 
 start_url = "https://5e.tools/bestiary.html#aarakocra%20simulacra_skt"
 gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))  #legt den driver in PATH ab
@@ -20,36 +17,21 @@ wait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='sta
 sleep(10)   #manchmal bekomme ich die Fehlermeldung, dass etwas noch nicht gefunden werden kann, hier pausiert das programm um sicher zu gehen, dass die seite geladen ist
 
 description = driver.find_element_by_xpath('//td[@class="text"]//descendant-or-self::*')   #xpath holt das gesuchte element
-
-name = driver.find_element_by_xpath("//span[@class='ecgen__name bold col-4-2 pl-0']/self::*")       #zeile bald überflüssig
-cr = driver.find_element_by_xpath("//span[@class='col-1-7 text-center']/self::*")                   #zeile bald überflüssig
-
-source_element = driver.find_element_by_xpath("//span[@class='col-2 text-center sourceMM pr-0']")
-source = source_element.get_attribute("title")
-
+                
 namelist = driver.find_elements_by_xpath("//span[@class='ecgen__name bold col-4-2 pl-0']")
 cr_list = driver.find_elements_by_xpath("//span[@class='col-1-7 text-center']")
-
-
-
-source_list = driver.find_elements_by_xpath("//span[starts-with(@title, 'col-2 text-center source')]")
-#for s in source_list:
-    #print(s.get_attribute("title"))            ###Test, bekomme noch nicht das richtige ergebniss
-
-
+source_list = driver.find_elements_by_xpath("//span[starts-with(@class, 'col-2')]")
 
 
 with open(r'.\Monsterliste.csv', mode='w') as monsterlist:
     writer = csv.DictWriter(monsterlist, fieldnames = ["Name", "CR", "Source", "Beschreibung"])
     writer.writeheader()
     monsterlist = csv.writer(monsterlist, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    monsterlist.writerow([name.text, cr.text, source, description.text])
+    
     for x in range(len(namelist)):
-        monsterlist.writerow([namelist[x].text, cr_list[x].text])  #bisher nur die beiden Attribute, bin schon am überlegen ob ich mir ne spider sparen kann :DD
+        monsterlist.writerow([namelist[x].text, cr_list[x].text, source_list[x].get_attribute("title")])  #bisher nur die beiden Attribute, bin schon am überlegen ob ich mir ne spider sparen kann :DD
         x += 1
 
 
 driver.close()
 #driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-#"//div[@class='rd__b  rd__b--2']/p[1]"
-####
