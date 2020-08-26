a9ed002af718e436693edef5deceeb6a53f6e380
RaHoRaHo <ralf.hoyer89@gmx.de>
2020-08-26 14:05:43 +0200

    Bekomme alle Links + einige Beschreibungen. Fehlt noch viel Feinarbeit!

--- Homebrew.py
+++ Homebrew.py
@@ -3,7 +3,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 
 
-# erst nur das grundgerüst
+
 
 class homebrew(scrapy.Spider):       #Spider
     name = "homebrew"
@@ -13,41 +13,37 @@ class homebrew(scrapy.Spider):       #Spider
         url = "https://www.dandwiki.com/wiki/5e_Monsters"     
         yield scrapy.Request(url=url, callback=self.get_urls)
  
-    def get_urls(self, response):        # hier wird der crawling prozess enstehen,  hatte leider nicht so viel zeit
-        odd_urls = response.xpath('//tr[@class = "odd"]//a/@href').getall()
-        even_urls = response.xpath('//tr[@class = "even"]//a/@href').getall()
-        for odd_link in odd_urls:
-            for even_link in even_urls:
-                yield response.follow(url=even_link, callback=self.get_all)
-            
-            yield response.follow(url=odd_link, callback=self.get_all)
+    def get_urls(self, response):         # holt sich alle links und übergibt an nächste funktion
+        urls = response.xpath('//tbody//child::*//a/@href').getall()
         
-        next_odd_page = response.xpath('//tr[@class = "odd"]//a/@href').get() # statt get() geht auch extract_first()
-        next_even_page = response.xpath('//tr[@class = "even"]//a/@href').get()
-        if next_odd_page is not None:
-            if next_even_page is not None:
-                yield response.follow(url=next_even_page, callback=self.get_urls)
-            yield response.follow(url=next_odd_page, callback=self.get_urls)
+        for link in urls:
+            yield response.follow(url=link, callback=self.get_all)
+        
+       # next_page = response.xpath('//tbody//child::*//a/@href').get()
+        
+      #  if next_page is not None:
+         #   yield response.follow(url=next_page, callback=self.get_urls)
         
         
         
           
         
-    def get_all(self, response):
-        name = response.xpath('//span[@class = "mw-headline"]')
-        print(name)
-        description = response.xpath('//table[@class = "5e"]//p//text()')
-        
-        
-        
+    def get_all(self, response):        # der xpath befehl funktioniert soweit. leider bekommt man nicht alle beschreibungen. manche sind noch extra verschachtelt in a tags z.b.
+        description = response.xpath('normalize-space(//table[@style = "margin: 1em auto 1em auto;"]//following::p//text())').extract()
+        import csv
+        import os
+        file_name = "Homebrewliste.csv"     #das funktioniert nicht wie es soll, hab aber heute keinen kopf mehr dafür! trennt jeden buchstaben mit , und überschreibt jedes mal den alten eintrag
+        with open(r'.\\' + file_name, mode='w') as homebrewlist:
+            writer = csv.DictWriter(homebrewlist, fieldnames = ["Name", "CR", "Source","URL", "Beschreibung"])
+            writer.writeheader()
+            homebrewlist = csv.writer(homebrewlist, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for x in range(len(description)):
+                homebrewlist.writerow(description[x])  
+                x += 1
+
+        # hier müssen noch name, cr, source, url eingefügt werden    
+                   
 
 process = CrawlerProcess()
 process.crawl(homebrew)
 process.start()
-#even_urls = response.xpath('//tr[@class = "even"]//a/@href').getall()
- #       for even_link in even_urls:
-#
- #           yield response.follow(url=even_link, callback=self.get_urls) 
-#next_even_page = response.xpath('//tr[@class = "even"]//a/@href').get() # statt get() geht auch extract_first()
- #       if next_even_page is not None:
-  #          yield response.follow(url=next_even_page, callback=self.get_urls) 
