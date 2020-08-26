cc848a21a1af29486dbba54ae9a825a9727ffb27
RaHoRaHo <ralf.hoyer89@gmx.de>
2020-08-26 16:05:49 +0200

    Kleine Tests gemacht, Namen hinzugefügt

--- Homebrew.py
+++ Homebrew.py
@@ -14,7 +14,7 @@ class homebrew(scrapy.Spider):       #Spider
         yield scrapy.Request(url=url, callback=self.get_urls)
  
     def get_urls(self, response):         # holt sich alle links und übergibt an nächste funktion
-        urls = response.xpath('//tbody//child::*//a/@href').getall()
+        urls = response.xpath('//table[@style = "width: 100%; text-align: left;"]//child::*//tr//a/@href').getall()
         
         for link in urls:
             yield response.follow(url=link, callback=self.get_all)
@@ -29,17 +29,24 @@ class homebrew(scrapy.Spider):       #Spider
           
         
     def get_all(self, response):        # der xpath befehl funktioniert soweit. leider bekommt man nicht alle beschreibungen. manche sind noch extra verschachtelt in a tags z.b.
-        description = response.xpath('normalize-space(//table[@style = "margin: 1em auto 1em auto;"]//following::p//text())').extract()
+        #description = response.xpath('normalize-space(//table[@style = "margin: 1em auto 1em auto;"]//following::p//text())').extract()
+        name = response.xpath('//h2/child::span/text()').extract()
+        
+       
+       
+       
         import csv
         import os
         file_name = "Homebrewliste.csv"     #das funktioniert nicht wie es soll, hab aber heute keinen kopf mehr dafür! trennt jeden buchstaben mit , und überschreibt jedes mal den alten eintrag
         with open(r'.\\' + file_name, mode='w') as homebrewlist:
+    
             writer = csv.DictWriter(homebrewlist, fieldnames = ["Name", "CR", "Source","URL", "Beschreibung"])
             writer.writeheader()
             homebrewlist = csv.writer(homebrewlist, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for x in range(len(description)):
-                homebrewlist.writerow(description[x])  
+            for x in range(len(name)):
+                homebrewlist.writerow(name[x])  
                 x += 1
+                continue
 
         # hier müssen noch name, cr, source, url eingefügt werden    
                    
