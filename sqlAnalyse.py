73f252a2633569d9e3218dc61b634318f6e5a558
Philipp Skoda <philipp2850@gmx.net>
2020-08-27 10:33:15 +0200

    sqlAnalyse gibt Buchtitel aus

--- sqlAnalyse.py
+++ sqlAnalyse.py
@@ -19,9 +19,10 @@ import sqlite3
 #addBookstoTable() Nur Initial einmal ausführen
 db = sqlite3.connect("Monster.db")
 c = db.cursor()
-c.execute("SELECT * FROM Monster")
-result =c.fetchone()
-print(result)
+c.execute("SELECT DISTINCT Bookname FROM Author")
+result =c.fetchall()
+for x in result:
+    print(x[0])
 
 #TODO: Zu den 14 Buchtitel die Autoren (und wenn du Bock hast) auch das Erscheinungsdatum scrapen
 #Wichtig: Damit das funktioniert, musst du 1mal vorher 5eTools.py ausgeführt haben, sonst bekommst du nich die Liste mit den Büchernamen
