f961bc5e34388349fef21be2ce61cca1feae7e37
RaHoRaHo <ralf.hoyer89@gmx.de>
2020-08-27 13:08:22 +0200

    Spider jetzt mit DNDBeyond. Fehlt nur noch der Zugang

--- Homebrew.py
+++ Homebrew.py
@@ -1,4 +1,4 @@
-
+import csv
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
@@ -10,47 +10,44 @@ class homebrew(scrapy.Spider):       #Spider
     
     
     def start_requests(self):       #Standard Funktion
-        url = "https://www.dandwiki.com/wiki/5e_Monsters"     
+        url = "https://www.dndbeyond.com/homebrew/monsters"     
         yield scrapy.Request(url=url, callback=self.get_urls)
  
     def get_urls(self, response):         # holt sich alle links und übergibt an nächste funktion
-        urls = response.xpath('//table[@style = "width: 100%; text-align: left;"]//child::*//tr//a/@href').getall()
         
+        urls = response.xpath('//a[@class = "link"]/@href').getall()
         for link in urls:
+            print(link)
             yield response.follow(url=link, callback=self.get_all)
         
-       # next_page = response.xpath('//tbody//child::*//a/@href').get()
-        
-      #  if next_page is not None:
-         #   yield response.follow(url=next_page, callback=self.get_urls)
+        next_page = response.xpath('//a[text()="Next"]/@href').get()       # holt sich den link für die nächte seite und übergibt wieder an sich selbst
+        if next_page is not None:
+           yield response.follow(url=next_page, callback=self.get_urls)
         
         
         
           
         
-    def get_all(self, response):        # der xpath befehl funktioniert soweit. leider bekommt man nicht alle beschreibungen. manche sind noch extra verschachtelt in a tags z.b.
-        description = response.xpath('normalize-space(//table[@style = "margin: 1em auto 1em auto;"]//following::p//text())').extract()
-        name = response.xpath('//h2/child::span/text()').extract()
+    def get_all(self, response):       
         
+        name = response.xpath('//div[@class = "mon-stat-block__name"]//a/text()').extract()
+        cr = response.xpath('//span[@class = "mon-stat-block__tidbit-label"]/text()').extract()
+        description = response.xpath('//div[@class = "mon-details__description-block-content"]//following::p/text()').extract()
+        autor = response.xpath('//div[@class = "source source-description"]/text()').extract()
+        url = response.xpath('//div[@class = "mon-stat-block__name"]//a/@href').get()
        
-       
-       
-        import csv
-        import os
-        file_name = "Homebrewliste.csv"     #das funktioniert nicht wie es soll, hab aber heute keinen kopf mehr dafür! trennt jeden buchstaben mit , und überschreibt jedes mal den alten eintrag
-        with open(r'.\\' + file_name, mode='w') as homebrewlist:
+        
+
+        file_name = "Homebrewliste.csv"    #header kann nicht geschrieben werden, sonst wäre der zwischen jeder zeile drin
+        with open(r'.\\' + file_name, mode='a') as homebrewlist:
     
-            writer = csv.DictWriter(homebrewlist, fieldnames = ["Name", "CR", "Source","URL", "Beschreibung"])
-            writer.writeheader()
+           # writer = csv.DictWriter(homebrewlist, fieldnames = ["Name", "CR", "Source","URL", "Beschreibung"])
+            #writer.writeheader()
             homebrewlist = csv.writer(homebrewlist, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             
-            homebrewlist.writerow(name)
-            homebrewlist.writerow(description)  
-            
-
-        # hier müssen noch name, cr, source, url eingefügt werden    
-                   
-
+            homebrewlist.writerow([name, cr, autor, url.get_attribute("href"), description])
+              
+                             
 process = CrawlerProcess()
 process.crawl(homebrew)
 process.start()
