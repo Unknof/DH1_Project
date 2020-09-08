05e6a586cc829b69e2d44a59d0cb76a8cd39523e
DEF <laura.gehr@web.de>
2020-09-08 11:28:47 +0200

    Problem mit SQL: Tabelle wird nicht gefüllt

--- namensStatistik.py
+++ namensStatistik.py
@@ -2,6 +2,7 @@
 import csv
 from collections import Counter
 import re
+from operator import itemgetter
 
 filename = 'Monsterliste.csv'
 
@@ -19,16 +20,20 @@ with open(filename) as csvfile: #Datei öffnen
             
             if re.search(r"/", crString): #Brüche
                 CRBruch = crString.split("/")
-                return int(CRBruch[0]) / int(CRBruch[1])
-                
-            elif re.search(r"—", crString): #keine Angabe => 0 Wert --> sollen noch raussortiert werden für meine Auswertung
-                CRNullwert = int(0)
-                return CRNullwert
-
-            else: #alle "normalen" Zahlen
-                CRZahl = int(crString)
+                CRZahl = float(CRBruch[0]) / float(CRBruch[1])
                 return CRZahl
 
+            elif re.search(r"—", crString): #keine Angabe => 0 Wert --> sollen noch raussortiert werden für meine Auswertung
+                CRZahl = float(0)
+                return CRZahl
+            elif re.search(r"Unknown", crString): #keine Angabe => 0 Wert --> sollen noch raussortiert werden für meine Auswertung
+                CRZahl = float(0)
+                return CRZahl
+
+            else: #alle "normalen" Zahlen
+                CRZahl = float(crString)
+                return CRZahl
+            #sorted(names, key=itemgetter(CRZahl))#sorted(names, key=CRZahl) --> sortieren funktioniert noch nicht
         if re.search(r"\s", name): #alle Namen mit Leerzeichen werden aussortiert
             names.remove(name)
        
@@ -36,8 +41,37 @@ with open(filename) as csvfile: #Datei öffnen
             #names.remove(crString)
 
         else:
-            print(row['Name'], CRUmwandler(crString))            #row['CR']) #für die restlichen Namen beginnt die Auswertung
+            
+            #print(row['Name'], CRUmwandler(crString))            #row['CR']) #für die restlichen Namen beginnt die Auswertung
             c = Counter(cleanerName)
-            print (c.most_common(5))
+            #print (c.most_common(5))
 
+
+        def namensStatistikTable():
+            import sqlite3
+            db = sqlite3.connect("Monster.db")
+            c = db.cursor()
+            c.execute("DROP TABLE IF EXISTS namensStatistikTable")
+            c.execute("""CREATE TABLE namensStatistikTable
+            (Name TEXT, CR FLOAT)""")
+
+        def insertData(Name, CR):
+            import sqlite3
+            namensStatistikTable()
+            db = sqlite3.connect("Monster.db")
+            c = db.cursor()
+            data = (name, CRUmwandler(crString))
+            c.execute("""
+            INSERT INTO namensStatistikTable (Name, CR) 
+            VALUES (?,?)""", data)
+
+            db.commit()
+            db.close()
+    
+        import sqlite3
+        db = sqlite3.connect("Monster.db")
+        c = db.cursor()
+        c.execute("SELECT * FROM namensStatistikTable")
+        result =c.fetchall()
+        print(result)
 
